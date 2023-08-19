cfc34b66bf025ec816b53f7fe5c42a2150c7c435
Gnlow <gnlow@protonmail.com>
2023-08-19 09:27:04 +0900

    mumumumu

--- 2.ts
+++ 2.ts
@@ -1,10 +1,17 @@
 class Publisher<T> {
     data
+    eventHandlers: ((item: T) => void)[]
     constructor(data: T[] = []) {
         this.data = data
+        this.eventHandlers = []
     }
-    push(item: T) {
-        this.data.push(item)
+    push(...items: T[]) {
+        items.forEach(
+            item => {
+                this.data.push(item)
+                this.eventHandlers.forEach(handler => handler(item))
+            }
+        )
     }
     subscribe() {
         // deno-lint-ignore no-this-alias
@@ -16,11 +23,30 @@ class Publisher<T> {
             },
             peak() {
                 return that.data[this.pointer]
+            },
+            onPush(handler: (item: T) => void) {
+                that.eventHandlers.push(handler)
             }
         }
     }
 }
 
-const join = (a: Publisher<string>, b: Publisher<string>) => {
-
+const join = (as: Publisher<string>, bs: Publisher<string>) => {
+    const cs = new Publisher<string[]>()
+    as.subscribe().onPush(a => cs.push(...bs.data.map(b => [a, b])))
+    bs.subscribe().onPush(b => cs.push(...as.data.map(a => [a, b])))
+    return cs
 }
+
+const as = new Publisher<string>()
+const bs = new Publisher<string>()
+const cs = join(as, bs)
+
+as.push("a", "b")
+bs.push("x", "y")
+console.log(cs.data)
+
+let pat = new Publisher<string>()
+const aa = new Publisher<string>()
+aa.push("a")
+pat.push
